476a85909dddfe146366745d3e6639601acf876c
subhan-dev <subhan.developer24@gmail.com>
2019-07-31 10:42:07 +0700

    update /

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) => {
+    res.status(200).send('<h1> Selamat Datang </h1>')
+})
+
 app.listen(port, () => {
     console.log('Berhasil Running di_ ' + port);
 })
